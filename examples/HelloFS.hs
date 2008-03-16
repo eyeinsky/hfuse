44d1df7145f08dbd9e5df944dae2a2980ec7517f
Will Thompson <will@willthompson.co.uk>
2008-03-16 22:27:01 +1100

    Update HelloFS to implement fuseReadDirectory.
    darcs-hash:20080316112701-4210b-e109a694ca3e3a48766d871af5c3b8fc403198a5.gz

--- examples/HelloFS.hs
+++ examples/HelloFS.hs
@@ -15,65 +15,75 @@ main = fuseMain helloFSOps defaultExceptionHandler
 
 helloFSOps :: FuseOperations HT
 helloFSOps = defaultFuseOps { fuseGetFileStat = helloGetFileStat
-                            , fuseGetDirectoryContents = helloGetDirectoryContents
                             , fuseOpen        = helloOpen
                             , fuseRead        = helloRead 
+                            , fuseOpenDirectory = helloOpenDirectory
+                            , fuseReadDirectory = helloReadDirectory
                             }
 helloString :: B.ByteString
 helloString = B.pack "Hello World, HFuse!\n"
 
 helloPath :: FilePath
 helloPath = "/hello"
+dirStat ctx = FileStat { statEntryType = Directory
+                       , statFileMode = foldr1 unionFileModes
+                                          [ ownerReadMode
+                                          , ownerExecuteMode
+                                          , groupReadMode
+                                          , groupExecuteMode
+                                          , otherReadMode
+                                          , otherExecuteMode
+                                          ]
+                       , statLinkCount = 2
+                       , statFileOwner = fuseCtxUserID ctx
+                       , statFileGroup = fuseCtxGroupID ctx
+                       , statSpecialDeviceID = 0
+                       , statFileSize = 4096
+                       , statBlocks = 1
+                       , statAccessTime = 0
+                       , statModificationTime = 0
+                       , statStatusChangeTime = 0
+                       }
+
+fileStat ctx = FileStat { statEntryType = RegularFile
+                        , statFileMode = foldr1 unionFileModes
+                                           [ ownerReadMode
+                                           , groupReadMode
+                                           , otherReadMode
+                                           ]
+                        , statLinkCount = 1
+                        , statFileOwner = fuseCtxUserID ctx
+                        , statFileGroup = fuseCtxGroupID ctx
+                        , statSpecialDeviceID = 0
+                        , statFileSize = fromIntegral $ B.length helloString
+                        , statBlocks = 1
+                        , statAccessTime = 0
+                        , statModificationTime = 0
+                        , statStatusChangeTime = 0
+                        }
 
 helloGetFileStat :: FilePath -> IO (Either Errno FileStat)
 helloGetFileStat "/" = do
     ctx <- getFuseContext
-    return $ Right $ FileStat { statEntryType = Directory
-                              , statFileMode = foldr1 unionFileModes
-                                                 [ ownerReadMode
-                                                 , ownerExecuteMode
-                                                 , groupReadMode
-                                                 , groupExecuteMode
-                                                 , otherReadMode
-                                                 , otherExecuteMode
-                                                 ]
-                              , statLinkCount = 2
-                              , statFileOwner = fuseCtxUserID ctx
-                              , statFileGroup = fuseCtxGroupID ctx
-                              , statSpecialDeviceID = 0
-                              , statFileSize = 4096
-                              , statBlocks = 1
-                              , statAccessTime = 0
-                              , statModificationTime = 0
-                              , statStatusChangeTime = 0
-                              }
+    return $ Right $ dirStat ctx
 helloGetFileStat path | path == helloPath = do
     ctx <- getFuseContext
-    return $ Right $ FileStat { statEntryType = RegularFile
-                              , statFileMode = foldr1 unionFileModes
-                                                 [ ownerReadMode
-                                                 , groupReadMode
-                                                 , otherReadMode
-                                                 ]
-                              , statLinkCount = 1
-                              , statFileOwner = fuseCtxUserID ctx
-                              , statFileGroup = fuseCtxGroupID ctx
-                              , statSpecialDeviceID = 0
-                              , statFileSize = fromIntegral $ B.length helloString
-                              , statBlocks = 1
-                              , statAccessTime = 0
-                              , statModificationTime = 0
-                              , statStatusChangeTime = 0
-                              }
+    return $ Right $ fileStat ctx
 helloGetFileStat _ =
     return $ Left eNOENT
 
-helloGetDirectoryContents :: FilePath -> IO (Either Errno [(FilePath, EntryType)])
-helloGetDirectoryContents "/" =
-    return $ Right [ (".",  Directory)
-                   , ("..", Directory)
-                   , (tail helloPath, RegularFile) ]
-helloGetDirectoryContents _ = return (Left (eNOENT))
+helloOpenDirectory "/" = return eOK
+helloOpenDirectory _   = return eNOENT
+
+helloReadDirectory :: FilePath -> IO (Either Errno [(FilePath, FileStat)])
+helloReadDirectory "/" = do
+    ctx <- getFuseContext
+    return $ Right [(".",          dirStat  ctx)
+                   ,("..",         dirStat  ctx)
+                   ,(helloName,    fileStat ctx)
+                   ]
+    where (_:helloName) = helloPath
+helloReadDirectory _ = return (Left (eNOENT))
 
 helloOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Errno, HT)
 helloOpen path mode flags
